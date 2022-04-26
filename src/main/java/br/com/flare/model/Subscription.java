a136639f4ff792bda4f32d8a838e4cf69dd97c34
Hugo Oliveira Soares <hugoliveira.soares@gmail.com>
2022-04-26 09:27:11 -0300

    Adicionando a restrição de unicidade ao bando de dados

--- src/main/java/br/com/flare/model/Subscription.java
+++ src/main/java/br/com/flare/model/Subscription.java
@@ -1,6 +1,11 @@
 package br.com.flare.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(name="subscriptions") // A tabela ira se chamar subscriptions, cada linha representa uma subscription
@@ -10,7 +15,7 @@ public class Subscription {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false) // Restrição no banco em que o campo não pode ser nulo
+  @Column(nullable = false, unique = true) // Restrição no banco em que o campo não pode ser nulo e deve ser unico
   private String token;
 
   public Subscription() {
