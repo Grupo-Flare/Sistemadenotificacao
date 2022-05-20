c3aa0cf2670b25d58b728fbf069edfc8a257a6bf
Diego Souza <diegosouzaprofissional3@gmail.com>
2022-05-20 09:02:27 -0300

    arrumando gerenciar usuario

--- src/main/resources/static/js/gerenciarUsuarios.js
+++ src/main/resources/static/js/gerenciarUsuarios.js
@@ -60,3 +60,16 @@ function validaAtualiza() {
 
     formulario.submit();
 }
+
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
